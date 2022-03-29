6eecaaa93e585d8ba6b870d5a4be71038239d785
Quentin Conner <quentin.conner@gmail.com>
2022-03-29 15:08:03 -0500

    updates for attempt at assembly

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,21 +3,15 @@ import sbt._
 trait Libraries {
   lazy val scalaTest          = "org.scalatest"                  %% "scalatest"              % "3.1.1"    % Test
 
-  // typesafe config
-  lazy val typesafeConfig     = "com.typesafe"                    % "config"                 % "1.4.2"
+  // Apache Spark 3.2.x
+  lazy val sparkCore          = "org.apache.spark"               %% "spark-core"             % "3.2.1"
 
-  // Apache Spark
-  lazy val sparkCore          = "org.apache.spark"               %% "spark-core"             % "3.2.1" // "2.4.8"
-
-  // why do I need this for core Spark?
-  //lazy val hadoopCommon       = "org.apache.hadoop"              %  "hadoop-common"          % "3.3.1"  % "provided"
-  lazy val hadoopClient       = "org.apache.hadoop"              %  "hadoop-client-api"      % "3.3.1"
-  //lazy val hadoopAWS          = "org.apache.hadoop"              %  "hadoop-aws"             % "3.3.1"
-  // /home/quentin/src/spark-example-rdd/target/bg-jobs/sbt_43efb294/target/6a8adcaa/81ecd14d/hadoop-client-api-3.3.1.jar
+  // Hadoop 3.2.x
+  lazy val hadoopClient       = "org.apache.hadoop"              %  "hadoop-client-api"      % "3.2.1"
 }
 
 object Dependencies extends Libraries {
-  lazy val allSpecified = Seq(
+  lazy val projectDependencies = Seq(
     scalaTest,
     sparkCore,
     hadoopClient
